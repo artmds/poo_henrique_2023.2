383e680b783dfdddf2d8c74f6c67b9857db522b1
artmds <arthur.mdsousa@gmail.com>
2023-10-03 23:48:08 -0300

    Finalização da classe Círculo

--- projeto2/mini-projeto-2.py
+++ projeto2/mini-projeto-2.py
@@ -1,14 +1,26 @@
 class Pontos():
     
-    def __init__(self, a, b, c):
-        self.a = a
-        self.b = b
-        self.c = c
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
     
     def printPontos(self):
-        print(f'\n Os pontos inseridos foram {self.a}, {self.b}, {self.c}')
+        print(f'\nOs pontos inseridos foram {self.x}, {self.y}, {self.z}')
         
-
+        
+class Circulo(Pontos):
+    def __init__(self, x, y, raio):
+        self.x = x
+        self.y = y
+        self.raio = raio
+    
+    def printaCirculo(self):
+        print(f'\nInformações do Círculo: Origem em X:{self.x}, Y:{self.y}, Raio: {self.raio}')
+    
+    def calculaArea(self):
+        print ('A área do seu Círculo é: ',3.14 * self.raio  / 2)
+        
 
 
 
@@ -19,10 +31,11 @@ def Main():
     pt1 = Pontos(4, 6, 8)
     pt1.printPontos()
     
-    
+    circulo1 = Circulo(4, 8, 24)
+    circulo1.printaCirculo()
+    circulo1.calculaArea()
     
     
     
 if __name__ == "__main__":
-
     Main()
