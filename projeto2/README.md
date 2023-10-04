c5c072931e6ed03476a30f92164ca7976e059a51
artmds <arthur.mdsousa@gmail.com>
2023-10-04 00:54:59 -0300

    :tada: alterações no readme, criação da classe triangulo, retangulo eajustes na formatação

--- projeto2/README.md
+++ projeto2/README.md
@@ -1,1 +1,7 @@
 Inicialização do repositório do mini projeto 2 que consiste em transformar o pseudocódigo do projeto 1 em um algoritmo funcional na linguagem Python.
+
+Conta com a classe (Ponto, Circulo, Quadrado e Triangulo)
+
+Possui métodos para inserir as informações das formas geométricas, e, a partir dessas informações, fazer cálculos básicos, que neste algoritmo, é informado apenas a área de cada forma
+
+🥳
--- projeto2/mini-projeto-2.py
+++ projeto2/mini-projeto-2.py
@@ -1,48 +1,72 @@
 class Pontos():
     
     def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+        self._x = x
+        self._y = y
+        self._z = z
     
     def printPontos(self):
-        print(f'\nOs pontos inseridos foram {self.x}, {self.y}, {self.z}')
+        print(f'\nOs pontos inseridos foram {self._x}, {self._y}, {self._z}')
         
 #Classe Circulo
 class Circulo(Pontos):
-    def __init__(self, x, y, raio):
-        self.x = x
-        self.y = y
-        self.raio = raio
+    def __init__(self, raio):
+        self._raio = raio
     
     def printaCirculo(self):
-        print(f'\nInformações do Círculo: Origem em X:{self.x}, Y:{self.y}, Raio: {self.raio}')
+        print(f'\nInformações do Círculo: Raio: {self._raio}')
     
     def calculaArea(self):
-        print ('A área do seu Círculo é: ',3.14 * self.raio  / 2)
+        print ('A área do seu Círculo é: ',3.14 * self._raio  / 2)
         
 #Classe Quadrado
 class Quadrado(Pontos):
     def __init__(self, lado):
-        self.lado = lado
+        self._lado = lado
+    
     
     def printaQuadrado(self):
-        print(f'\nSeu quadrado possui lado: {self.lado}')
+        print(f'\nSeu quadrado possui possui lado:{self._lado}')
+        
         
     def calculaAreaQuadrado(self):
-        print ('A área do seu quadrado é: ', self.lado ** 2)
+        print ('A área do seu quadrado é: ', self._lado ** 2)
 
 
+class Triangulo(Pontos):
+    def __init__(self, b, h):
+        self._b = b
+        self._h = h
+    
+    def printaTriangulo(self):
+        print(f'\nSeu Triângulo possui base:{self._b} e altura:{self._h}')
+    
+    def calculaAreaTriangulo(self):
+        print("A área do seu triângulo é de:", self._b * self._h / 2)
 
+
+class Retangulo(Pontos):
+    def __init__(self, b, h):
+        self._b = b
+        self._h = h
+    
+    def printaRetangulo(self):
+        print(f'\nSeu Retângulo possui base:{self._b} e altura:{self._h}')
+    
+    def calculaAreaRetangulo(self):
+        print("A área do seu reângulo é de:", self._b * self._h)
 
 
 def Main():
+    
+    print("\nOlá, seja bem vindo! :)\n")
+    
     pt1 = Pontos(4, 6, 8)
     pt1.printPontos()
     
     print("-------------\n")
     
-    circulo1 = Circulo(4, 8, 24)
+    circulo1 = Circulo(24)
     circulo1.printaCirculo()
     circulo1.calculaArea()
     
@@ -52,6 +76,19 @@ def Main():
     quadrado1.printaQuadrado()
     quadrado1.calculaAreaQuadrado()
     
+    print("-------------\n")
     
+    triangulo1 = Triangulo(5, 5)
+    triangulo1.printaTriangulo()
+    triangulo1.calculaAreaTriangulo()
+    
+    print("-------------\n")
+
+    retangulo1 = Retangulo(5, 2)
+    retangulo1.printaRetangulo()
+    retangulo1.calculaAreaRetangulo()
+    
+    print("-------------\n")
+
 if __name__ == "__main__":
     Main()
