a90cf7258c1ac621a21af3e3c73f312af934a6fd
artmds <arthur.mdsousa@gmail.com>
2023-10-03 23:55:25 -0300

    classe quadrado finalizada

--- projeto2/mini-projeto-2.py
+++ projeto2/mini-projeto-2.py
@@ -8,7 +8,7 @@ class Pontos():
     def printPontos(self):
         print(f'\nOs pontos inseridos foram {self.x}, {self.y}, {self.z}')
         
-        
+#Classe Circulo
 class Circulo(Pontos):
     def __init__(self, x, y, raio):
         self.x = x
@@ -21,7 +21,16 @@ class Circulo(Pontos):
     def calculaArea(self):
         print ('A área do seu Círculo é: ',3.14 * self.raio  / 2)
         
-
+#Classe Quadrado
+class Quadrado(Pontos):
+    def __init__(self, lado):
+        self.lado = lado
+    
+    def printaQuadrado(self):
+        print(f'\nSeu quadrado possui lado: {self.lado}')
+        
+    def calculaAreaQuadrado(self):
+        print ('A área do seu quadrado é: ', self.lado ** 2)
 
 
 
@@ -31,10 +40,17 @@ def Main():
     pt1 = Pontos(4, 6, 8)
     pt1.printPontos()
     
+    print("-------------\n")
+    
     circulo1 = Circulo(4, 8, 24)
     circulo1.printaCirculo()
     circulo1.calculaArea()
     
+    print("-------------\n")
+    
+    quadrado1 = Quadrado(4)
+    quadrado1.printaQuadrado()
+    quadrado1.calculaAreaQuadrado()
     
     
 if __name__ == "__main__":
